b26b4e59b16c67768a9b9d3563cfa5f5b63ef509
alexd <alex@dreamisdead.tk>
2017-03-24 15:48:18 +0300

    Initial commit
    Upload to GitHub

---
+++ mountainduck.py
@@ -0,0 +1,71 @@
+import sys
+import os
+import getpass
+import xml.etree.ElementTree as ET
+import Alfred
+
+handler = Alfred.Handler(sys.argv, use_no_query_string=False)
+
+f = open("mountainduck_settings_folder.txt", "r")
+settings_folder = f.read()
+
+# remove the \n
+settings_folder = settings_folder[:-1]
+f.close()
+
+settings_folder = settings_folder.replace("${username}", getpass.getuser())
+
+settings_folder = os.path.join(settings_folder, "Bookmarks")
+
+bookmark_info = []
+
+for files in os.listdir(settings_folder):
+	if files.endswith(".duck"):
+		keys = []
+		values = []
+
+		file_path = os.path.join(settings_folder, files)
+
+		tree = ET.parse(file_path)
+
+		root = tree.getroot()[0]
+
+		for child in root:
+			if child.tag == "key":
+				keys.append(child.text)
+			elif child.tag == "string":
+				values.append(child.text)
+
+		bookmark_info.append({"keys": keys, "values": values, "file_path": file_path})
+
+for info in bookmark_info:
+	nickname = ""
+	username = ""
+	port = ""
+	protocol = ""
+	hostname = ""
+	file_path = info["file_path"]
+
+	i = 0
+
+	for key in info["keys"]:
+		if key == "Nickname":
+			nickname = info["values"][i]
+		elif key == "Username":
+			username = info["values"][i]
+		elif key == "Port":
+			port = info["values"][i]
+		elif key == "Protocol":
+			protocol = info["values"][i]
+		elif key == "Hostname":
+			hostname = info["values"][i]
+
+		i += 1
+
+	if handler.query == "" or handler.query.lower() in nickname.lower():
+		handler.add_new_item(title=nickname, subtitle="%s %s@%s:%s" % (protocol.upper(), username, hostname, port), icon="icon.png", arg=nickname)
+
+if len(os.listdir(settings_folder)) == 0:
+	handler.add_new_item(title="No bookmarks found at", subtitle="%s" % settings_folder, icon="bookmark_icon.png")
+
+handler.push()
